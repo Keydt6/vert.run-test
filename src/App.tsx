d02745c35aacf5a85a1f0d5f5ceb01b9144a7360
Keyvin Diaz <keyvin.95@gmail.com>
2024-01-08 21:39:01 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AthleteActivitiesList from './components/AthleteActivitiesList'
+function App() {
+  return (
+    <>
+      <h1>Vent.run Test</h1>
+
+      <AthleteActivitiesList />
+    </>
+  )
+}
+
+export default App
